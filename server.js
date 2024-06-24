7ea1ce82d2c4b21e3738bcb83887e67d6ecc34d3
卄卂几ㄩ爪卂 丂卂丨 Ҝ尺丨丂卄几卂 <hanumasaikrishna21@gmail.com>
2024-06-24 17:36:53 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const app = express()
-const session = require('express-session')
+const session=require('express-session')
 const PORT = process.env.PORT || 3030;
 // const cors = require('cors')
 // app.use(cors())
@@ -27,7 +27,7 @@ app.get('/', (req, res) => {
   // res.redirect(`/${uuidV4()}`)
 })
 
-app.get('/room', (req, res) => {
+app.get('/room', (req,res) => {
   res.redirect(`/${uuidV4()}`)
 })
 
@@ -84,7 +84,7 @@ io.on('connect', socket => {
 
   socket.on('erase', (data) => {
     connections.forEach((con) => {
-      if (con.id !== socket.id) {
+      if(con.id !== socket.id) {
         con.emit('onErase', { x: data.x, y: data.y })
       }
     })
@@ -92,7 +92,7 @@ io.on('connect', socket => {
 
   socket.on('clear', () => {
     connections.forEach((con) => {
-      if (con.id !== socket.id) {
+      if(con.id !== socket.id) {
         con.emit('onClear')
       }
     })
@@ -115,7 +115,7 @@ io.on('connect', socket => {
 app.use(session({
   resave: false,
   saveUninitialized: true,
-  secret: 'SECRET'
+  secret: 'SECRET' 
 }));
 
 // app.get(`/${uuidV4()}`, function(req, res) {
@@ -132,35 +132,35 @@ app.set('view engine', 'ejs');
 app.get('/success', (req, res) => res.send(userProfile));
 app.get('/error', (req, res) => res.send("error logging in"));
 
-passport.serializeUser(function (user, cb) {
+passport.serializeUser(function(user, cb) {
   cb(null, user);
 });
 
-passport.deserializeUser(function (obj, cb) {
+passport.deserializeUser(function(obj, cb) {
   cb(null, obj);
 });
 
 const GoogleStrategy = require('passport-google-oauth').OAuth2Strategy;
-const GOOGLE_CLIENT_ID = '';
+const GOOGLE_CLIENT_ID = '912338031006-042ul6dqblg2p88efp2m3evmrj3s4fk9.apps.googleusercontent.com';
 
-const GOOGLE_CLIENT_SECRET = '';
+const GOOGLE_CLIENT_SECRET = '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';
 passport.use(new GoogleStrategy({
-  clientID: GOOGLE_CLIENT_ID,
-  clientSecret: GOOGLE_CLIENT_SECRET,
-  callbackURL: "http://localhost:3030/auth/google/callback"
-},
-  function (accessToken, refreshToken, profile, done) {
-    userProfile = profile;
-    return done(null, userProfile);
+    clientID: GOOGLE_CLIENT_ID,
+    clientSecret: GOOGLE_CLIENT_SECRET,
+    callbackURL: "http://localhost:3030/auth/google/callback"
+  },
+  function(accessToken, refreshToken, profile, done) {
+      userProfile=profile;
+      return done(null, userProfile);
   }
 ));
-
-app.get('/auth/google',
-  passport.authenticate('google', { scope: ['profile', 'email'] }));
-
-app.get('/auth/google/callback',
+ 
+app.get('/auth/google', 
+  passport.authenticate('google', { scope : ['profile', 'email'] }));
+ 
+app.get('/auth/google/callback', 
   passport.authenticate('google', { failureRedirect: '/error' }),
-  function (req, res) {
+  function(req, res) {
     // Successful authentication, redirect success.
     //res.redirect('/success');
     res.render('home')
